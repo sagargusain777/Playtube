172fe0622e46c09f099e76b0426d04f36d6c9d04
uwuxverticuse <sagargusain150@gmail.com>
2024-05-17 09:30:43 +0530

    ✅ Added a route middleware

--- src/app.js
+++ src/app.js
@@ -16,4 +16,11 @@ app.use(express.static('public'))
 
 app.use(cookieParser());
 
+
+//Routes import
+
+import userRouter from "./routes/user.routes.js";
+
+//routes Declaration
+app.use('/api/v1/users',userRouter);
 export default app;
